24ea4b4c80d182bcefb18f33411ac241468e7543
wolfmarcos <79427305+wolfmarcos@users.noreply.github.com>
2023-01-27 20:13:28 -0300

    33

--- src/hooks/firebase-config.js
+++ src/hooks/firebase-config.js
@@ -24,13 +24,13 @@ import {
 import { useEffect, useState } from "react";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAq9AOg-sxa35Z2EflWYSg6AJb6NSXR2gU",
-  authDomain: "cotizador-616a3.firebaseapp.com",
-  projectId: "cotizador-616a3",
-  storageBucket: "cotizador-616a3.appspot.com",
-  messagingSenderId: "657272668279",
-  appId: "1:657272668279:web:cc6f0fa016682995d00672",
-  measurementId: "G-FDG9SJ15XN",
+  apiKey: "-",
+  authDomain: "-..com",
+  projectId: "-",
+  storageBucket: "-616a3..com",
+  messagingSenderId: "",
+  appId: "1::web:",
+  measurementId: "G-",
 };
 
 const app = initializeApp(firebaseConfig);
